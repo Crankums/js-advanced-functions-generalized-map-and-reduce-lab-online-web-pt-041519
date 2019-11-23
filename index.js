8de9839619b2e3979c8037718a6d7a41e0ccfcb1
Craig Donavin <crdonavin@gmail.com>
2019-11-23 21:22:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ function map(src, fn) {
 
 function reduce(src, start = 0) {
   let arr =[]
-
-
+  if (start) {
+    let total = 0
+    for (let i=start; i < src.length; i++) {
+      total += src[i]
+    }
+    return total
+  } else {
+    let total = 0
+    for (let i = 0; i < src.length; i++) {
+      total+= src[i]
+    }
+    return total
+  }
 }
