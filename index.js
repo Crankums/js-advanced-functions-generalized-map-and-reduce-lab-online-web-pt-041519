caa2e8da338ddb42456a3dd902f8b1b17eb2f05e
Craig Donavin <crdonavin@gmail.com>
2019-11-23 21:08:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your functions here
-function map(src) {
-
+function map(src, val) {
+    let arr = []
+    for (let i=0; i<src.length; i++) {
+      let el = src[i]
+      arr.puch(val(el))
+    }
+    return arr
 }
